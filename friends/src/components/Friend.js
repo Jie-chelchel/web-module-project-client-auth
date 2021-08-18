32cc9a6ad641f438db928630951fad9cf943328a
Jie Zhang <zhangjiew@gmail.com>
2021-08-17 19:12:56 -0600

    added css

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Friend = (props) => {
   return (
-    <div className="auth">
+    <div className="auth friend">
       <Link to={`/friends/${props.friend.id}`}>
         <h2>Name :{props.friend.name}</h2>
       </Link>
